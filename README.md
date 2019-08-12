60d82813f92b09187509560313fea4f28f4186f5
Austin Lasseter <austinlasseter@gmail.com>
2019-08-12 09:38:26 -0400

    updated app

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to deploy a simple Plotly Dash app on Heroku
+
+A full explanation is available on [Medium](https://medium.com/@austinlasseter/how-to-deploy-a-simple-plotly-dash-app-to-heroku-622a2216eb73).
+
+You can view the finished app on [Heroku](https://flying-dog.herokuapp.com/).
+
+**Note:** Plotly Dash apps can only be viewed in a modern browser (like Chrome or Mozilla). They won't render in antediluvian browsers such as Microsoft.
---
+++ app.py
@@ -0,0 +1,46 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
+########### Set up the chart
+
+myfavoritecolor='C74A2F'
+x_list=['giraffes', 'elephants', 'monkeys']
+y_list=y=[5, 2, 7]
+
+data = [go.Bar(
+            x=x_list,
+            y=y_list,
+            marker=dict(color=myfavoritecolor)
+    )]
+
+layout = go.Layout(
+    title = 'My favorite animals', # Graph title
+    xaxis = dict(title = 'Type of animal'), # x-axis label
+    yaxis = dict(title = 'Number in the zoo'), # y-axis label
+
+)
+fig = go.Figure(data=data, layout=layout)
+
+
+########### Display the chart
+
+app = dash.Dash()
+server = app.server
+
+app.layout = html.Div(children=[
+    html.H1('Animals in the Zoo'),
+    dcc.Graph(
+        id='figure-1',
+        figure=fig
+    ),
+    html.A('Code on Github', href='https://github.com/austinlasseter/zoo-animals-dash'),
+    html.A('HTML Color Codes', href='https://htmlcolorcodes.com'),
+
+
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server()
